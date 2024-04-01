235e01ad44af84e4027d48d4199d2fbe7381f3ee
Hatem DAGHBOUJ <hatemdagbouj@gmail.com>
2024-04-01 15:50:22 +0200

    modify views: change users view

---
+++ views/templates/scaffold/menu.blade.php
@@ -0,0 +1,6 @@
+<li class="c-sidebar-nav-item">
+    <a class="c-sidebar-nav-link" href="{{ route('users.index') }}" class="{{ Request::is('users*') ? 'c-active' : '' }}">
+        <i class="c-sidebar-nav-icon cil-list"></i>
+        <span>Users</span>
+    </a>
+</li>

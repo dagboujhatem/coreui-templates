c8dfed487717db50de2f4cd1d9e132f48e4fa479
Hatem DAGHBOUJ <hatemdagbouj@gmail.com>
2024-04-01 15:04:27 +0200

    modify views: change menu icon

--- views/templates/layouts/menu_template.blade.php
+++ views/templates/layouts/menu_template.blade.php
@@ -1,6 +1,6 @@
 <li class="c-sidebar-nav-item">
     <a class="c-sidebar-nav-link" href="@{{ route('{!! $config->prefixes->getRoutePrefixWith('.') !!}{!! $config->modelNames->camelPlural !!}.index') }}" class="@{{ Request::is('{!! $config->prefixes->getRoutePrefixWith('.') !!}{!! $config->modelNames->camelPlural !!}*') ? 'c-active' : '' }}">
-        <i class="c-sidebar-nav-icon cil-home"></i>
+        <i class="c-sidebar-nav-icon cil-list"></i>
         @if($config->options->localized)
             <span>@@lang('models/{{ $config->modelNames->camelPlural }}.plural')</span>
         @else
